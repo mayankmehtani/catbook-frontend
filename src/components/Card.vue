dbf37a32a74866acb8343cb92a35745a8beee2e0
mayankmehtani <mayank.mehtani@gmail.com>
2023-01-08 22:22:00 +0000

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <div id="pic">
+            <img src="../assets/moh.jpg"/>
+        </div>
+
+        <div id="cat-info">
+            <div id="field-headers">
+                <div v-if="name">Name: </div>
+                <div>Age: </div>
+
+
+                <div v-if="neuteredStatus">
+                    <div>Neutered: </div>
+                    <div>Date Caught: </div>
+                    <div>Date Neutered: </div>
+                    <div>Date Released: </div>
+                </div>
+                <div>Lane: </div>
+                <div>Phase: </div>
+            </div>
+
+            <div id="field-values">
+                <div>{{ name }}</div>
+                <div>{{ age }} months</div>
+ 
+                <div v-if="neuteredStatus">
+                    <div>{{ neuteredStatus }}</div>
+                    <div>{{ dateCaught }} </div>
+                    <div>{{ dateNeutered }} </div>
+                    <div>{{ dateReleased }}</div>
+                </div>
+                <div>{{ Lane }}</div>
+                <div>{{ Phase }}</div>
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: "Card",
+    props: {
+        name: {
+            type: String,
+            required: true
+        }
+    }
+};
+</script>
+
+
+
+<style scoped>
+#pic > img {
+    height: 370px;
+    width: 500px;
+}
+
+#cat-info {
+    flex-direction: row;
+    display: flex;
+    justify-content: space-evenly;
+}
+
+#field-values {
+    text-align: right;
+    flex-direction: ;
+}
+</style>
---
+++ src/views/CatInfo.vue
@@ -0,0 +1,42 @@
+<template>
+    <Card :name="name">
+    </Card>
+</template>
+
+<script lang="ts">
+    import Card from '../components/Card.vue';
+    import axios from 'axios';
+
+    export default {
+      name: "CatInfo",
+      data() {
+        name: String
+        return { 
+          name: "a"
+        }
+      },
+      components: {Card},
+        async beforeMount () {
+          let res = await axios.get("http://0.0.0.0:8000/cat/1/")
+          this.name = res["data"]["name"]
+          
+          
+          
+      }
+      // beforeMount(
+      //   () => 
+      
+      // )
+    }
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
